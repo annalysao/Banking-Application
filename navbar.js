702949789f2a50ee9204b90155b0c06d4597c5f3
Annalysa Owen <100535460+annalysao@users.noreply.github.com>
2022-09-16 14:10:39 -0400

    navbar set up and context for the account.
    issue when click on bad bank in the navbar- the server says it is not available and the whole page goes to an error. the other pages in the navbar work.

---
+++ navbar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return (
+        <>
+        <a href="#">Bad Bank</a>,
+        <a href="#/createAccount/">Create Account</a>,
+        <a href="#/login/">Login</a>,
+        <a href="#/deposit">Deposit</a>,
+        <a href="#/withdraw">Withdraw</a>,
+        <a href="#/balance">Balance</a>,
+        <a href="#/alldata">AllData</a>,
+        </>
+    )
+}
