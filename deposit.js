a4154da467762ccd022bfef104c339e3b94d85de
Annalysa Owen <100535460+annalysao@users.noreply.github.com>
2022-09-17 22:21:51 -0400

    Navbar is mostly complete. cards set up
    need to add functionality to withdraw, deposit, and balance. need to add hover effect Navbar

--- deposit.js
+++ deposit.js
@@ -1,8 +1,46 @@
 function Deposit(){
+    const [show, setShow]           = React.useState(true);
+    const [status, setStatus]       = React.useState('');
+    const [deposit, setDeposit]     = React.useState('');
     const ctx = React.useContext(UserContext);
+
+    function validate(field, label){
+        if (!field) {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleCreate(){
+        console.log(deposit);
+        if (!validate(deposit,     'deposit')) return;
+        ctx.users.push({deposit});
+        setShow(false);
+    }
+
+    function clearForm(){
+        setDeposit('');
+        setShow(true);
+    }
+
     return (
-        <h1>Deposit<br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
+        <Card
+            bgcolor="info"
+            header="Deposit"
+            status={status}
+            body={show ? (
+                <>
+                <input type="input" className="form-control" id="deposit" placeholder="Enter deposit amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} />
+                <button type="submit" className="btn btn-light" onClick={handleCreate}>Deposit</button>
+                </>
+            ):(
+                <>
+                <h5>Success</h5>
+                <button type="submit" className="btn btn-light" onClick={clearForm}>Make another deposit</button>
+                </>
+            )}
+        />
+    )
 }
--- withdraw.js
+++ withdraw.js
@@ -1,8 +1,46 @@
 function Withdraw(){
+    const [show, setShow]           = React.useState(true);
+    const [status, setStatus]       = React.useState('');
+    const [withdraw, setWithdraw]   = React.useState('');
     const ctx = React.useContext(UserContext);
+
+    function validate(field, label){
+        if (!field) {
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''),3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleCreate(){
+        console.log(withdraw);
+        if (!validate(withdraw,     'withdraw')) return;
+        ctx.users.push({withdraw});
+        setShow(false);
+    }
+
+    function clearForm(){
+        setWithdraw('');
+        setShow(true);
+    }
+
     return (
-        <h1>Withdraw<br/>
-            {JSON.stringify(ctx)}
-        </h1>
-    );
+        <Card
+            bgcolor="info"
+            header="Withdraw"
+            status={status}
+            body={show ? (
+                <>
+                <input type="input" className="form-control" id="withdraw" placeholder="Enter withdrawl amount" value={withdraw} onChange={e => setWithdraw(e.currentTarget.value)} />
+                <button type="submit" className="btn btn-light" onClick={handleCreate}>Withdraw</button>
+                </>
+            ):(
+                <>
+                <h5>Success</h5>
+                <button type="submit" className="btn btn-light" onClick={clearForm}>Make another withdrawl</button>
+                </>
+            )}
+        />
+    )
 }
