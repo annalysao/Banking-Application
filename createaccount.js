35fe9b197856fa449bdd591f00e1666578b9068f
Annalysa Owen <100535460+annalysao@users.noreply.github.com>
2022-09-19 16:55:47 -0400

    button disabled if all three not complete

--- createaccount.js
+++ createaccount.js
@@ -1,9 +1,10 @@
 function CreateAccount(){
-    const [show, setShow]           = React.useState(true);
-    const [status, setStatus]       = React.useState('');
-    const [name, setName]           = React.useState('');
-    const [email, setEmail]         = React.useState('');
-    const [password, setPassword]   = React.useState('');
+    const [show, setShow]                   = React.useState(true);
+    const [status, setStatus]               = React.useState('');
+    const [name, setName]                   = React.useState('');
+    const [email, setEmail]                 = React.useState('');
+    const [password, setPassword]           = React.useState('');
+    const [formCompleted, setFormCompleted] = React.useState(false);
     const ctx = React.useContext(UserContext);
 
     function validate(field, label){
@@ -28,8 +29,13 @@ function CreateAccount(){
         setName('');
         setEmail('');
         setPassword('');
+        setFormCompleted(false);
         setShow(true);
     }
+    
+    function inspectFormStatus() {
+        setFormCompleted(name && email && password);
+      }
 
     return (
         <Card
@@ -39,17 +45,60 @@ function CreateAccount(){
             body={show ? (
                 <>
                 Name<br/>
-                <input type="input" className="form-control" id="name" placeholder="Enter name"  value={name} onChange={e => setName(e.currentTarget.value)} required/><br/>
+                <input 
+                    type="input" 
+                    className="form-control"
+                    id="name" 
+                    placeholder="Enter name"  
+                    value={name} 
+                    onChange={e => {
+                        setName(e.currentTarget.value);
+                        inspectFormStatus()
+                        }}
+                    onBlur={(e) => inspectFormStatus()}
+                    /><br/>
                 Email address<br/>
-                <input type="input" className="form-control" id="email" placeholder="Enter email"  value={email} onChange={e => setEmail(e.currentTarget.value)} required/><br/>
+                <input 
+                    type="input" 
+                    className="form-control"
+                    id="email" 
+                    placeholder="Enter email"  
+                    value={email} 
+                    onChange={e => {
+                        setEmail(e.currentTarget.value); 
+                        inspectFormStatus()}
+                    }
+                    onBlur={(e) => inspectFormStatus()} 
+                    /><br/>
                 Password<br/>
-                <input type="password" required className="form-control" id="password" placeholder="Enter password"  minlength="8" value={password} onChange={e => setPassword(e.currentTarget.value)} /><br/>
-                <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+                <input 
+                    type="password" 
+                    required className="form-control"
+                    id="password" 
+                    placeholder="Enter password"  
+                    minlength="8" 
+                    value={password} 
+                    onChange={e => {
+                        setPassword(e.currentTarget.value);
+                        inspectFormStatus();
+                    }} 
+                    onBlur={(e) => inspectFormStatus()}   
+                    /><br/>
+                <button 
+                    type="submit" 
+                    className="btn btn-light" 
+                    onClick={handleCreate}
+                    disabled={!formCompleted}
+                    >Create Account
+                </button>
                 </>
             ):(
                 <>
                 <h5>Success</h5>
-                <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+                <button 
+                type="submit" 
+                className="btn btn-light" 
+                onClick={clearForm}>Add another account</button>
                 </>
             )}
         />
