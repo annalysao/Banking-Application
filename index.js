3484825d09e694bd1d1b427cd39fb13dfab5d3fb
Annalysa Owen <100535460+annalysao@users.noreply.github.com>
2023-01-07 16:18:41 -0500

    adding code for logout function

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function Spa(){
                 <Route path="/" exact component={Home} />
                 <Route path="/createaccount/" component={CreateAccount} />
                 <Route path="/login/" component={Login} />
+                <Route path="/logout/" component={Logout} />
                 <Route path="/deposit/" component={Deposit} />
                 <Route path="/withdraw/" component={Withdraw} />
                 <Route path="/balance/" component={Balance} />
